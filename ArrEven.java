6e8b6a7236b4f1e1fe17612aca52a785eddde22b
kopurisunil <53226020+kopurisunil@users.noreply.github.com>
2019-07-24 16:54:09 +0530

    first

---
+++ ArrEven.java
@@ -0,0 +1,21 @@
+
+public class ArrEven {
+
+	public static void main(String[] args) {
+		int arr[] = { 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		
+	System.out.println("oddnomber:");
+	for(int i=0;i<arr.length;i++) {
+		if(arr[i]%2==1);
+		System.out.println(arr[i]);
+	}
+	
+		}
+			
+
+		
+	
+}
+
+	
+
